cd1273110b6ae9c53ce91f22b1a33e45d067a670
Abhishek Bohora <abhishek.bohora16@gmail.com>
2024-08-22 12:23:18 +0545

    enable cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createServer } from "http";
-
+import cors from "cors";
 import userRouter from "./routes/auth/user.routes.js";
 import categoryRouter from "./routes/category.routes.js";
 import productRouter from "./routes/product.routes.js";
@@ -9,6 +9,10 @@ import cartRouter from "./routes/cart.routes.js";
 
 const app = express();
 const httpServer = createServer(app);
+
+// Enable CORS for all routes
+app.use(cors());
+
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
